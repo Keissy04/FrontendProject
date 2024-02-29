1e364cd06758cfb64eb3be88db0ad897d3fbc8f2
Keissy04 <keissyrabenjamina@gmail.com>
2024-02-29 11:54:20 +0300

    First init

---
+++ src/app/api.url.ts
@@ -0,0 +1,15 @@
+export const apiUrl = {
+    authServiceApi: "https://backend-production-cb4b.up.railway.app/users/"
+}
+export const apiUrlservice = {
+    ServiceApi: "https://backend-production-cb4b.up.railway.app/services/"
+}
+export const apiUrlRendezVous = {
+    RendezVousApi: "https://backend-production-cb4b.up.railway.app/rendez_vous/"
+}
+export const apiUrlPreference = {
+    preferenceApi: "https://backend-production-cb4b.up.railway.app/preference/"
+}
+export const apiUrlStat = {
+    StatApi: "https://backend-production-cb4b.up.railway.app/stats/"
+}
